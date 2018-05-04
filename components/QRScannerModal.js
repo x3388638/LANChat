b53eacaf4ff7564a35610319802428e1883c6810
YY <z3388638@gmail.com>
2018-05-04 18:06:42 +0800

    add react-native-qrcode-scanner to QRScannerModal; but the react-native-camera version not work on android

--- components/QRScannerModal.js
+++ components/QRScannerModal.js
@@ -2,19 +2,59 @@ import React from 'react';
 import {
 	View,
 	Text,
+	TouchableOpacity,
+	Linking,
+	StyleSheet,
 	AsyncStorage
 } from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export default class HomeScreen extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 
+	onSuccess(e) {
+		Linking
+			.openURL(e.data)
+			.catch(err => console.error('An error occured', err));
+	}
+
 	render() {
 		return (
-			<View>
-				<Text style={{ marginTop: 50 }}>aaaa</Text>
-			</View>
+			<QRCodeScanner
+				onRead={this.onSuccess.bind(this)}
+				topContent={
+					<Text style={styles.centerText}>
+						Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+          			</Text>
+				}
+				bottomContent={
+					<TouchableOpacity style={styles.buttonTouchable}>
+						<Text style={styles.buttonText}>OK. Got it!</Text>
+					</TouchableOpacity>
+				}
+			/>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	centerText: {
+		flex: 1,
+		fontSize: 18,
+		padding: 32,
+		color: '#777',
+	},
+	textBold: {
+		fontWeight: '500',
+		color: '#000',
+	},
+	buttonText: {
+		fontSize: 21,
+		color: 'rgb(0,122,255)',
+	},
+	buttonTouchable: {
+		padding: 16,
+	},
+});
