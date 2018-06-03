cd57a9253911bdebb129f8f5adce6fd82708d934
YY <z3388638@gmail.com>
2018-06-04 00:06:33 +0800

    ui for MoreFuncModal

--- components/MoreFuncModal.js
+++ components/MoreFuncModal.js
@@ -2,8 +2,12 @@ import React from 'react';
 import {
 	View,
 	Text,
+	TouchableOpacity,
 	StyleSheet
 } from 'react-native';
+import {
+	Divider
+} from 'react-native-elements';
 import Modal from 'react-native-modal';
 
 export default class MoreFuncModal extends React.Component {
@@ -15,7 +19,23 @@ export default class MoreFuncModal extends React.Component {
 				onBackdropPress={ this.props.hide }
 			>
 				<View style={ styles.container }>
-					<Text>123</Text>
+					<TouchableOpacity
+						onPress={() => {}}
+					>
+						<Text style={styles.btn}>發起投票</Text>
+					</TouchableOpacity>
+					<Divider />
+					<TouchableOpacity
+						onPress={() => {}}
+					>
+						<Text style={styles.btn}>選擇圖片</Text>
+					</TouchableOpacity>
+					<Divider />
+					<TouchableOpacity
+						onPress={() => { }}
+					>
+						<Text style={styles.btn}>選擇檔案</Text>
+					</TouchableOpacity>
 				</View>
 			</Modal>
 		)
@@ -24,6 +44,18 @@ export default class MoreFuncModal extends React.Component {
 
 const styles = StyleSheet.create({
 	container: {
-		backgroundColor: '#fff'
+		backgroundColor: '#fff',
+		paddingTop: 20,
+		paddingBottom: 20,
+		paddingLeft: 10,
+		paddingRight: 10,
+		borderRadius: 20
+	},
+	btn: {
+		textAlign: 'center',
+		color: '#007aff',
+		fontSize: 30,
+		paddingTop: 8,
+		paddingBottom: 8
 	}
 });
