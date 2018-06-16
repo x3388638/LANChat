53c381adf20daccc8583d507ccceef248671cb08
YY <z3388638@gmail.com>
2018-06-16 22:19:44 +0800

    improve input value handler at CreateGroupScreen

--- components/CreateGroupScreen.js
+++ components/CreateGroupScreen.js
@@ -27,7 +27,6 @@ export default class CreateGroupScreen extends React.Component {
 			generating: false
 		};
 
-		this.handleChangeText = this.handleChangeText.bind(this);
 		this.handleCreate = this.handleCreate.bind(this);
 	}
 
@@ -35,22 +34,8 @@ export default class CreateGroupScreen extends React.Component {
 		title: '新群組'
 	};
 
-	handleChangeText(key, value, eventType) {
-		if (eventType === 1 && Platform.OS === 'ios') {
-			return;
-		}
-
-		if (eventType === 2 && Platform.OS !== 'ios') {
-			return;
-		}
-
-		this.setState({
-			[key]: value
-		});
-	}
-
 	handleCreate() {
-		Alert.alert('群組建立中...', null, [{ text: 'OK', onPress: Keyboard.dismiss }]);
+		Keyboard.dismiss();
 		this.setState({
 			generating: true
 		});
@@ -65,7 +50,7 @@ export default class CreateGroupScreen extends React.Component {
 				Alert.alert('欄位不得為空');
 				return;
 			}
-			
+
 			const key = Util.genGroupKey(groupName, pass);
 			const groupID = Util.genUUID();
 			const createdTime = moment().format();
@@ -88,7 +73,7 @@ export default class CreateGroupScreen extends React.Component {
 			this.setState({
 				generating: false
 			});
-		}, 300);
+		}, 150);
 	}
 
 	render() {
@@ -96,27 +81,22 @@ export default class CreateGroupScreen extends React.Component {
 			<KeyboardAwareScrollView style={ styles.container }>
 				<FormLabel>群組名稱</FormLabel>
 				<FormInput
-					ref={(node) => {this.groupName = node}}
-					value={ this.state.groupName }
 					maxLength={15}
-					onChangeText={(text) => { this.handleChangeText('groupName', text, 1) }}
-					onEndEditing={(e) => { this.handleChangeText('groupName', e.nativeEvent.text, 2) }}
+					value={ Platform.OS === 'ios' ? null : this.state.groupName }
+					onChangeText={(groupName) => { this.setState({ groupName }) }}
 				/>
 				<FormLabel>群組密碼</FormLabel>
 				<FormInput
-					ref={(node) => {this.pass = node}}
-					value={ this.state.pass }
 					maxLength={20}
-					onChangeText={(text) => { this.handleChangeText('pass', text, 1) }}
-					onEndEditing={(e) => { this.handleChangeText('pass', e.nativeEvent.text, 2) }}
+					value={ Platform.OS === 'ios' ? null : this.state.pass }
+					onChangeText={(pass) => { this.setState({ pass }) }}
 				/>
 				<FormLabel>群組簡介</FormLabel>
 				<FormInput
-					ref={(node) => { this.groupDesc = node }}
-					value={this.state.groupDesc}
+					multiline
 					maxLength={150}
-					onChangeText={(text) => { this.handleChangeText('groupDesc', text, 1) }}
-					onEndEditing={(e) => { this.handleChangeText('groupDesc', e.nativeEvent.text, 2) }}
+					value={ Platform.OS === 'ios' ? null : this.state.groupDesc }
+					onChangeText={(groupDesc) => { this.setState({ groupDesc }) }}
 				/>
 				<View style={ styles.btnContainer }>
 					<Button
